f89540ab673b8a133b3a94ad6c8bdc88bf70d360
Linhpvt <linhpvt60@gmail.com>
2021-08-05 15:08:16 +0700

    update URI for both host and remote application

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonCfg = require('./webpack.common');
+
 const domain = process.env.PRODUCTION_DOMAIN || 'http://localhost';
 const pkgDependencies = require('../package.json');
 
@@ -17,7 +18,8 @@ const prodConfg = {
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        // all files will be deployed under /marketing/lastest folder in S3 bucket
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: pkgDependencies.dependencies,
     }),
--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -8,6 +8,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
+    publicPath: '/marketing/lastest/',
   },
   plugins: [
     new ModuleFederationPlugin({
